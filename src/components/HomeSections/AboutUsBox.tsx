5392735c6f92177afa5be7a64d341927c06a5c64
Emmanueloye <esunday963@gmail.com>
2025-03-29 16:59:06 +0100

    update

--- src/components/HomeSections/AboutUsBox.tsx
+++ src/components/HomeSections/AboutUsBox.tsx
@@ -1,4 +1,4 @@
-import phone from '../../assets/Single_Phone_nobg.png';
+import phone from '../../assets/mobile-noBg.png';
 const AboutUsBox = () => {
   return (
     <div className='pt-8 dark:bg-slate-800' id='solutions'>
@@ -10,10 +10,30 @@ const AboutUsBox = () => {
         </h1>
         {/*=================================================================================
            ========================= About us section==================================== */}
-        <div>
+        <div className='grid grid-cols-2'>
           <img src={phone} alt='Letango phone' />
-          <div>
-            <div></div>
+          <div className='bg-green-500 w-full h-full relative'>
+            <div className='h-full w-full bg-slate-50 dark:bg-slate-700 border-2 absolute -top-2 -left-2 p-2 dark:text-slate-50'>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              <br />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
           </div>
         </div>
       </section>
